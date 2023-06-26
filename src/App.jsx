a7a3ef5e014e878ab1c0112e848eb1a1a6fdf3dd
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-06-26 10:51:15 +0530

    Blogcard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+<<<<<<< Updated upstream
 import Tags from "./components/Tags";
 
 function App() {
   return <div className=""></div>;
+=======
+// import env from "../config/env";
+import { useState, useEffect } from "react";
+import fetchBlog from "../util/fetchBlog";
+import BlogCard from "./components/BlogCard";
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  const getBlogs = async () => {
+    const blogsFromServer = await fetchBlog("http://localhost:8080/random");
+    setBlogs(blogsFromServer);
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
+  return (
+    <div>
+      <BlogCard blogs={blogs} />
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
