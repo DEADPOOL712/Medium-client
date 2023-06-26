6340a0e01613917c6f056bd05df2b5e4c7e642a2
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-06-26 12:51:37 +0530

    follow button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-<<<<<<< Updated upstream
-import Tags from "./components/Tags";
-
-function App() {
-  return <div className=""></div>;
-=======
 // import env from "../config/env";
 import { useState, useEffect } from "react";
 import fetchBlog from "../util/fetchBlog";
 import BlogCard from "./components/BlogCard";
+import FollowButton from "./components/FollowButton";
 
 function App() {
-  const [blogs, setBlogs] = useState([]);
-
-  const getBlogs = async () => {
-    const blogsFromServer = await fetchBlog("http://localhost:8080/random");
-    setBlogs(blogsFromServer);
-  };
-
-  useEffect(() => {
-    getBlogs();
-  }, []);
-
   return (
-    <div>
-      <BlogCard blogs={blogs} />
+    <div className="flex mx-5 my-5 gap-5">
+      <h1>App page!</h1>
+      <FollowButton />
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
---
+++ src/components/FollowButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FollowButton() {
+  return (
+    <div className="px-3 py-1 border border-black flex rounded-full ">
+      <p>Follow</p>
+    </div>
+  );
+}
+
+export default FollowButton;
