7ebe8335df3b3b4578790ca63d1194b68ee52273
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-06-27 20:17:03 +0530

    navbar complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
-import { Children } from "react";
-
-const Button = ({ children, bgColor, textColor }) => {
+const Button = ({ children, bgColor, textColor, hoverEffect, rounded }) => {
   return (
     <div className="flex justify-center items-center">
       <button
-        className={"rounded-full bg-green-600 text-[14px] py-2 px-3 leading-4"}
+        className={`text-[14px] py-2 px-3 leading-4 
+        ${bgColor ? bgColor : ""} 
+        ${textColor ? textColor : ""}
+        ${rounded ? "rounded-full" : ""}
+        ${hoverEffect ? hoverEffect : ""}
+      `}
       >
         {children}
       </button>
